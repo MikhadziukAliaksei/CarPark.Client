4d3658e00788d341c7e2c9674213b489f1483cfd
MikhadziukAliaksei <69195770+MikhadziukAliaksei@users.noreply.github.com>
2021-05-03 15:12:35 +0300

    initial proj

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import withRoot from './withroot';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import AuthService from "./services/auth.service";
+
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/home.component";
+import {Header} from "./components/header.component";
+import AppAppBar from "./views/AppAppBar";
+import SignIn from './SignIn';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user
+      });
+    }
+  }
+
+  logOut() {
+    AuthService.logout();
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+    
+    return (
+      <div>
+        <AppAppBar />
+        <Header  user={currentUser} showModerator={showModeratorBoard} showAdmin={showAdminBoard}/>
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={SignIn} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRoot(App);
---
+++ client/src/components/header.component.js
@@ -0,0 +1,65 @@
+import {Link} from 'react-router-dom'
+
+
+export const Header = (props) => {
+    const {user, showModerator, showAdmin} = props
+    return (
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+            {showModerator && (
+              <li className="nav-item">
+                <Link to={"/mod"} className="nav-link">
+                  Moderator Board
+                </Link>
+              </li>
+            )}
+
+            {showAdmin && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )}
+
+            {user && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+
+          {user ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+        
+    )
+}
